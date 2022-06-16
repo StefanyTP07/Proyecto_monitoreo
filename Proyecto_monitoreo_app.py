85142d04d921a23b1c7331d9ab7303d1fc0c9a1b
StefanyTP07 <106336762+StefanyTP07@users.noreply.github.com>
2022-06-16 11:38:22 -0500

    Update Proyecto_monitoreo_app.py

--- Proyecto_monitoreo_app.py
+++ Proyecto_monitoreo_app.py
@@ -9,10 +9,10 @@ def download_data():
   gdown.download(url,output, quiet= False)
 
 download_data()
-st.title('Monitoreo Miraflores')
-st.write('Tabla:')
-data=pd.read_csv('data.csv', sep=',', nrows=1000000, parse_dates=['Fecha', 'Longitud'])
-st.dataframe(data.head(20))
+#st.title('Monitoreo Miraflores')
+#st.write('Tabla:')
+#data=pd.read_csv('data.csv', sep=',', nrows=1000000, parse_dates=['Fecha', 'Longitud'])
+#st.dataframe(data.head(20))
 
 @st.experimental_memo
 def download2_data():
@@ -21,10 +21,10 @@ def download2_data():
   gdown.download(url,output, quiet= False)
 
 download2_data()
-st.title('Monitoreo Bonilla')
-st.write('Tabla:')
-data=pd.read_csv('data2.csv', sep=',', nrows=1000000, parse_dates=['Fecha', 'Longitud'])
-st.dataframe(data.head(20))
+#st.title('Monitoreo Bonilla')
+#st.write('Tabla:')
+#data=pd.read_csv('data2.csv', sep=',', nrows=1000000, parse_dates=['Fecha', 'Longitud'])
+#st.dataframe(data.head(20))
 
 import matplotlib.pyplot as plt
 import pandas as pd
