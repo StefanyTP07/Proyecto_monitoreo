00e63253b151c6d78a10cbc345e6e75fa8cf1c32
StefanyTP07 <106336762+StefanyTP07@users.noreply.github.com>
2022-06-16 00:22:13 -0500

    Update Proyecto_monitoreo_app.py

--- Proyecto_monitoreo_app.py
+++ Proyecto_monitoreo_app.py
@@ -9,6 +9,8 @@ def download_data():
   gdown.download(url,output, quiet= False)
 
 download_data()
+st.title('Monitoreo Miraflores')
+st.write('Tabla:')
 data=pd.read_csv('data.csv', sep=',', nrows=1000000, parse_dates=['Fecha', 'Longitud'])
 st.dataframe(data.head(20))
 
