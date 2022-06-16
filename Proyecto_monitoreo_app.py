defc9518286f52fbe379d39ed14ec58938f5c07b
StefanyTP07 <106336762+StefanyTP07@users.noreply.github.com>
2022-06-15 23:13:45 -0500

    Create Proyecto_monitoreo_app.py

---
+++ Proyecto_monitoreo_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import gdown
+
+@st.experimental_memo
+def download_data():
+  url = 'https://docs.google.com/uc?id=1tReqZLXKH569JkzNQ7cc8kTFA11UdN6qI2PgvDvE6zs'
+  output = 'data.csv'
+  gdown.download(url,output, quiet= False)
+  return filename
+
+download_data()
+data=pd.read_csv('data.csv', sep=';', nrows=1000000, parse_dates=['Fecha', 'Longitud'])
+st.dataframe(data.head(20))
